6a862502fb01b67f0043995d2ddfbfc9b100367a
Stian Jensen <me@stianj.com>
2016-04-26 18:56:56 +0200

    Add beer bottle

--- src/laptopLayer.js
+++ src/laptopLayer.js
@@ -11,6 +11,7 @@ function laptopLayer(layer) {
   this.scene.add(pointLight);
 
   var objLoader = new THREE.OBJLoader();
+  var mtlLoader = new THREE.MTLLoader();
   var that = this;
   Loader.loadAjax('res/laptop.obj', function(response) {
     that.laptop = objLoader.parse(response);
@@ -27,6 +28,31 @@ function laptopLayer(layer) {
     that.sunglasses.position.set(0, 2, 0);
     that.scene.add(that.sunglasses);
   });
+  var bottleContainer = {};
+  Loader.loadAjax('res/bottle.mtl', function(response) {
+    bottleContainer.mtl = response;
+    if (bottleContainer.obj) {
+      finishBottleLoading(bottleContainer);
+    }
+  });
+  Loader.loadAjax('res/bottle.obj', function(response) {
+    bottleContainer.obj = response;
+    if (bottleContainer.mtl) {
+      finishBottleLoading(bottleContainer);
+    }
+  });
+
+  function finishBottleLoading(bottleContainer) {
+    var bottleMaterialCreator = mtlLoader.parse(bottleContainer.mtl);
+    bottleMaterialCreator.preload();
+
+    objLoader.setMaterials(bottleMaterialCreator);
+    that.bottle = objLoader.parse(bottleContainer.obj);
+    var scale = 2;
+    that.bottle.scale.set(scale, scale, scale);
+    that.bottle.position.set(0, -20, 0);
+    that.scene.add(that.bottle);
+  }
 
   this.renderPass = new THREE.RenderPass(this.scene, this.camera);
 }
@@ -53,6 +79,16 @@ laptopLayer.prototype.update = function(frame, relativeFrame) {
     this.sunglasses.rotation.z = 0.1 * Math.sin(relrelframe / 100 + speedup * relrelframe / 100);
     if(relrelframe > 250) {
       this.sunglasses.position.x = smoothstep(-10, -200, (relrelframe - 250) / 100);
+    }
+  }
+  if(this.bottle) {
+    var relrelframe = relativeFrame - 480;
+    var speedup = smoothstep(0.1, 4, relrelframe / 200);
+    this.bottle.position.x = smoothstep(200, -10, relrelframe / 100);
+    this.bottle.rotation.y =  0.3 * Math.sin(relrelframe / 100 + speedup * relrelframe / 100);
+    this.bottle.rotation.x = -Math.PI/2;// + 0.1 * Math.sin(relrelframe / 100 + speedup * relrelframe / 100);
+    if(relrelframe > 250) {
+      this.bottle.position.x = smoothstep(-10, -200, (relrelframe - 250) / 100);
     }
   }
 };
